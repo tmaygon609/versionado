d25462b792c13a29cc253dd81b84dfb5937dab1e
tmaygon609 <tmaygon609@iesalmudeyne.es>
2023-11-09 14:12:32 +0100

    Testeando el etiquetado de versiones

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+/**
+ * Muestra por pantalla la frase ¡Hola mundo! * 
+ */
+
+public class HolaMundo {
+    public static void main(String[] args){
+        System.out.prinln("¡Hola mundo!");
+    }
+}
