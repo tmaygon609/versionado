6602da0d72888239f16fd78bc38fbb577dd332a5
tmaygon609 <tmaygon609@iesalmudeyne.es>
2023-11-09 14:15:39 +0100

    Seguimos testeando el etiquetado de versiones

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,5 +5,6 @@
 public class HolaMundo {
     public static void main(String[] args){
         System.out.prinln("¡Hola mundo!");
+        System.out.prinln("Aquí estamos haciendo pruebas en Git.");
     }
 }
